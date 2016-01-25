7f887a9aa31531bf87b26f2111f814c7bf81d0b9
ChenXinGuang1003 <583860027@qq.com>
2016-01-25 11:37:38 +0800

    对账方式合并“新建提交”和“编辑提交”为“保存”

--- base/admin/reconcilType/views.py
+++ base/admin/reconcilType/views.py
@@ -7,7 +7,6 @@ from django.http import HttpResponseRedirect
 
 def reconcilType(request):
     #复选框列表
-
     conn2 = MethodUtil.getMysqlConn()
     conn2.autocommit(True)
     cur=conn2.cursor()
@@ -19,78 +18,93 @@ def reconcilType(request):
 
     #对账方式列表（页面左部）
     QsqlRec = "select id,rname from reconcil where status='1'"
-    QcurSel = conn2.cursor()
-    QcurSel.execute(QsqlRec)
-    QrecList = QcurSel.fetchall()
+    cur.execute(QsqlRec)
+    QrecList = cur.fetchall()
 
     JsqlRec = "select id,rname from reconcil where status='0'"
-    JcurSel = conn2.cursor()
-    JcurSel.execute(JsqlRec)
-    JrecList = JcurSel.fetchall()
+    cur.execute(JsqlRec)
+    JrecList = cur.fetchall()
 
+    #
+    rid = request.GET.get('rid','')
+    if rid:
+        sqlShow1="select id,rname,status from reconcil where id={rid}".format(rid=rid)
+        curShow1 = conn2.cursor()
+        curShow1.execute(sqlShow1)
+        reconList = curShow1.fetchone()
+
+        sqlShow2 = "select pid from reconcilitem where rid={rid}".format(rid=rid)
+        curShow2 = conn2.cursor()
+        curShow2.execute(sqlShow2)
+        payTyleLoad = curShow2.fetchall()
+    else:
+        reconList = []
+        payTyleLoad = []
+    #新建、修改、删除
     action = request.GET.get('action','')
     if request.method == 'POST':
         reconName = request.POST.get('reconName')
         rstatus =  request.POST.get('rstatus')
         payTyleList = request.POST.getlist('payType')
-        if action == 'new':
+        rid = request.POST.get('reconId')
+
+        if action == 'save':
+            #修改
+            if rid:
+                try:
+                    #更新reconcil数据
+                    sqlEdit1 = "update reconcil set rname='{rname}',status='{rstatus}' where id={id}"\
+                                .format(rname=reconName,rstatus=rstatus,id=rid)
+                    cur.execute(sqlEdit1)
+
+                    #删除相关信息
+                    sqlEdit2 = "delete from reconcilitem where rid={rid}".format(rid=rid)
+                    cur.execute(sqlEdit2)
+
+                    for payTpye in payTyleList:
+                        sqlEdit3 = "insert into reconcilitem (rid,pid) values({rid},'{pid}')".format(rid=rid,pid=payTpye)
+                        cur.execute(sqlEdit3)
+
+                    cur.close()
+                    conn2.close()
+                    succ = True
+                except:
+                    succ = False
+            #保存
+            else:
+                try:
+                    sqlNew1 = "insert into reconcil (rname,status) values('{name}','{status}')".format(name=reconName,status=rstatus)
+                    cur.execute(sqlNew1)
+
+                    sqlNew2 = "select id from reconcil where rname='{rname}'".format(rname=reconName)
+                    cur.execute(sqlNew2)
+                    rid = cur.fetchone()
+
+                    for payTpye in payTyleList:
+                        sqlNew3 = "insert into reconcilitem (rid,pid) values({rid},'{pid}')".format(rid=int(rid['id']),pid=payTpye)
+                        cur.execute(sqlNew3)
+
+                    cur.close()
+                    conn2.close()
+                    succ = True
+                except:
+                    succ = False
+        #删除
+        elif action == 'del':
             try:
-                sqlNew1 = "insert into reconcil (rname,status) values('{name}','{status}')".format(name=reconName,status=rstatus)
-                curNew1 = conn2.cursor()
-                print(sqlNew1)
-                curNew1.execute(sqlNew1)
-                curNew1.close()
+                rid = request.POST.get('reconId')
+                sqlDel1 = "delete from reconcil where id={id}".format(id=rid)
+                cur.execute(sqlDel1)
 
-                sqlNew2 = "select id from reconcil where rname='{rname}'".format(rname=reconName)
-                curNew2 = conn2.cursor()
-                curNew2.execute(sqlNew2)
-                rid = curNew2.fetchone()
-                curNew2.close()
+                sqlDel2 = "delete from reconcilitem where rid={rid}".format(rid=rid)
+                cur.execute(sqlDel2)
 
-                curNew3 = conn2.cursor()
-                for payTpye in payTyleList:
-                    sqlNew3 = "insert into reconcilitem (rid,pid) values({rid},'{pid}')".format(rid=int(rid['id']),pid=payTpye)
-                    curNew3.execute(sqlNew3)
-                curNew3.close()
+                succ = True
+                cur.close()
                 conn2.close()
-                succ = True
             except:
                 succ = False
-        elif action == 'edit':
-            #获取修改项目名称
-            rid = request.POST.get('reconId')
-            try:
-                #更新reconcil数据
-                sqlEdit1 = "update reconcil set rname='{rname}',status='{rstatus}' where id={id}"\
-                            .format(rname=reconName,rstatus=rstatus,id=rid)
-                curEdit1 = conn2.cursor()
-                curEdit1.execute(sqlEdit1)
-                curEdit1.close()
-                #删除相关信息
-                sqlEdit2 = "delete from reconcilitem where rid={rid}".format(rid=rid)
-                curEdit2 = conn2.cursor()
-                curEdit2.execute(sqlEdit2)
-                curEdit2.close()
 
-                curEdit3 = conn2.cursor()
-                for payTpye in payTyleList:
-                    sqlEdit3 = "insert into reconcilitem (rid,pid) values({rid},'{pid}')".format(rid=rid,pid=payTpye)
-                    curEdit3.execute(sqlEdit3)
-                succ = True
-            except:
-                succ = False
-    else:
-        rid = request.GET.get('rid','')
-        if rid:
-            sqlShow1="select id,rname,status from reconcil where id={rid}".format(rid=rid)
-            curShow1 = conn2.cursor()
-            curShow1.execute(sqlShow1)
-            reconList = curShow1.fetchone()
 
-            sqlShow2 = "select pid from reconcilitem where rid={rid}".format(rid=rid)
-            curShow2 = conn2.cursor()
-            curShow2.execute(sqlShow2)
-            payTyleLoad = curShow2.fetchall()
-            print(payTyleLoad)
 
     return render(request,'admin/reconcilType.html',locals())
