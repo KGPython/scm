35852a252c7820079ab8d416ab5e7e98f22f36c4
ChenXinGuang1003 <583860027@qq.com>
2016-01-30 10:27:41 +0800

    syslog修改

--- base/supplier/invioce/views.py
+++ base/supplier/invioce/views.py
@@ -152,13 +152,13 @@ def saveInvioce(request):
         conn.rollback()
     finally:
         conn.commit()
+    MethodUtil.insertSysLog(conn2,Constants.SCM_ACCOUNT_LOGINID,Constants.SCM_ACCOUNT_WORKSTATIONID,Constants.SCM_ACCOUNT_MODULEID,Constants.SCM_ACCOUNT_EVENTID[5],"")
 
     sqlFlow = "insert into sheetflow(sheetid,sheettype,flag,operflag,checker,checkno,checkdate,checkdatetime) " \
               "values('{shid}',{shType},{flag},{operFlag},'{checker}',{chNo},convert(char(10),getdate(),120),getdate())"\
               .format(shid=sheetId,shType=res2[0][2],flag=0,operFlag=0,checker=Constants.SCM_ACCOUNT_LOGINNO,chNo=Constants.SCM_ACCOUNT_LOGINID)
     cur.execute(sqlFlow)
-
-    MethodUtil.insertSysLog(conn2,Constants.SCM_ACCOUNT_LOGINID,Constants.SCM_ACCOUNT_WORKSTATIONID,Constants.SCM_ACCOUNT_MODULEID,Constants.SCM_ACCOUNT_EVENTID[5],"操作员:{suppCode}保存单据[{sheetId}]".format(suppCode=suppCode,sheetId=sheetId))
+    MethodUtil.insertSysLog(conn2,Constants.SCM_ACCOUNT_LOGINID,Constants.SCM_ACCOUNT_WORKSTATIONID,Constants.SCM_ACCOUNT_MODULEID,Constants.SCM_ACCOUNT_EVENTID[6],"操作员:{suppCode}保存单据[{sheetId}]".format(suppCode=suppCode,sheetId=sheetId))
     conn.commit()
     cur.close()
     conn.close()
