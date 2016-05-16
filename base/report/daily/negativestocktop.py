d8034b6452d00004ac5d48a6a3c7aa67daf9511a
chenxinguang1003 <583860027@qq.com>
2016-05-16 17:45:31 +0800

    负库存

--- base/report/daily/negativestocktop.py
+++ base/report/daily/negativestocktop.py
@@ -4,42 +4,51 @@ __author__ = 'liubf'
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from base.utils import MethodUtil
-import time,datetime
+import datetime
+from django.core import serializers
 
-@csrf_exempt
 def index(request):
+    #最近返回数据
+    dataList = []
+
     conn = MethodUtil.getMysqlConn()
     start = str(datetime.date.today().replace(day=1))
-    end = str(datetime.datetime.today())
-
+    today = str(datetime.datetime.today())
+    yesterday = str(datetime.datetime.today() - datetime.timedelta(days=1))
     #合计
-    sqlSum = "select shopid,shopname,SUM(qtyz) AS qtyz,SUM(qtyl) AS qtyl,(SUM(qtyl)/SUM(qtyz)) AS zhonbi from Fcu_Stock WHERE stockdate BETWEEN '"+start+"' AND '"+end+"' GROUP BY shopid order by shopid"
+    sqlSum = "select b.shopid,b.shopname,SUM(b.qtyz) AS qtyz,SUM(b.qtyl) AS qtyl,(SUM(b.qtyl)/SUM(b.qtyz)) AS zhonbi,(select count(distinct zhonbi) from Fcu_Stock a where a.zhonbi <= b.zhonbi) AS mingci from Fcu_Stock AS b WHERE stockdate BETWEEN '"+start+"' AND '"+today+"' GROUP BY shopid order by shopid"
+    print(sqlSum)
     cur = conn.cursor()
     cur.execute(sqlSum)
     sumList = cur.fetchall()
 
-    dataList = []
+    #将合计(横向)数据拼入数组
     for obj in sumList:
         list = []
-        list.append(obj)
+        list.append(obj['shopid'])
+        list.append(obj['shopname'])
+        list.append(str(obj['qtyz']))
+        list.append(str(obj['qtyl']))
+        list.append(str(obj['zhonbi']))
+        list.append(str(obj['mingci']))
         dataList.append(list)
 
-    mingci = 1
+    #求各门店明细并拼入数组
     dataLen = len(dataList)
-    mxList =[]
     for i in range(0,dataLen):
-        sql = "SELECT qtyz,qtyl,zhonbi,mingci FROM Fcu_Stock WHERE shopid ='"+sumList[i]['shopid']+"'"
+        sql = "SELECT qtyz,qtyl,zhonbi,mingci FROM Fcu_Stock WHERE shopid ='"+sumList[i]['shopid']+"' ORDER BY stockdate"
         cur.execute(sql)
         list = cur.fetchall()
-        dataList[i].append(list)
 
-        # sumList[i]['mingci'] = mingci
-        # if sumList[i]['zhonbi'] <= sumList[i-1]['zhonbi']:
-        #     mingci += 1
-        #     sumList[i]['mingci'] = mingci
+        for obj in list:
+            dataList[i].append(str(obj['qtyz']))
+            dataList[i].append(str(obj['qtyl']))
+            dataList[i].append(str(obj['zhonbi']))
+            dataList[i].append(str(obj['mingci']))
+    #底部合计（纵向）
+    sumList2 = []
 
-    print(dataList)
-    return render(request,'report/daily/negative_stock_top.html',locals())
+    return render(request,'report/daily/negative_stock_top.html',{"dataList":dataList})
 
 @csrf_exempt
 def query():
