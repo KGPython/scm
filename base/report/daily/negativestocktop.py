d6fda6d69e826295101c8eab7f03e17333d943a1
end-e <xiuliang555@foxmail.com>
2016-05-28 14:54:35 +0800

    xiul 负库存修复qtyl为0

--- base/report/daily/negativestocktop.py
+++ base/report/daily/negativestocktop.py
@@ -3,110 +3,88 @@ __author__ = 'liubf'
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from base.utils import MethodUtil
 import datetime
-from django.core import serializers
+from base.utils import MethodUtil
 
-
+@csrf_exempt
 def index(request):
-    # 返回数据
-    dataList = []
-
+    monthFirst = str(datetime.date.today().replace(day=1))
+    today = str(datetime.datetime.today().strftime('%y-%m-%d'))
     conn = MethodUtil.getMysqlConn()
-    start = str(datetime.date.today().replace(day=1))
-    today = str(datetime.datetime.today())
-    yesterday = str(datetime.date.today() - datetime.timedelta(days=1))
-    # 合计
-    sql = "select ShopID, shopname, " \
-          "sum(qtyz) as qtyz, " \
-          "sum(qtyl) as qtyl, " \
-          "(sum(qtyl) / sum(qtyz)) as zhonbi " \
-          "from KNegativestock " \
-          "where sdate between '" + start + "' and '" + today + \
-          "' group by ShopID " \
-          "order by ShopID asc "
+
+    sql = 'SElECT ShopID,shopname, SUM(qtyz) AS qtyzSum,SUM(qtyl) AS qtylSum,(sum(qtyl) / sum(qtyz)) AS zhonbiSum ' \
+          'FROM KNegativestock ' \
+          'WHERE sdate BETWEEN "'+monthFirst+'" AND "'+today+'" GROUP BY ShopID ORDER BY ShopID'
     cur = conn.cursor()
     cur.execute(sql)
-    sumList = cur.fetchall()
-
-    print(sumList)
-
-    try:
-        if (sumList):
-            pass
-    except:
-        pass
-
-    print(sumList)
-
-    # 将合计(横向)数据拼入数组
-    for obj in sumList:
-        list = []
-        list.append(obj['ShopID'])
-        list.append(obj['shopname'])
-        # qtyz 有效商品数
-        list.append(str(obj['qtyz']))
-        # qtyl 合计
-        list.append(str(obj['qtyl']))
-        # zhonbi  占比
-        list.append(format(obj['zhonbi'] * 100, '0.2f'))
-        # list.append(format((obj['zhonbi']), '0.2%'))
-        dataList.append(list)
-
-    # 调用ranking()函数对占比进行排名
-    dataList = ranking(dataList)
-
-    # 再调整排序，按照ShopID
-    dataList.sort()
-    # print(dataList)
-
-    # 求各门店明细并拼入数组
-    # dataLen = len(dataList)
-    # for i in range(0, dataLen):
-    #     sql = "SELECT b.ShopID, b.sdate, b.qtyz, b.qtyl, b.zhonbi, (SELECT COUNT(DISTINCT zhonbi) FROM KNegativestock a WHERE a.zhonbi <= b.zhonbi) AS mingci " \
-    #           "FROM KNegativestock AS b " \
-    #           "WHERE ShopID ='" + sumList[i]['ShopID'] + \
-    #           "' ORDER BY sdate"
-    #     print(sql)
-    #     cur.execute(sql)
-    #     list = cur.fetchall()
-    #
-    #     for obj in list:
-    #         dataList[i].append(str(obj['qtyz']))
-    #         dataList[i].append(str(obj['qtyl']))
-    #         dataList[i].append(str(obj['zhonbi']))
-    #         dataList[i].append(str(obj['mingci']))
-    # 底部合计（纵向）
-    # sumList2 = []
-
-    return render(request, 'report/daily/negative_stock_top.html', {"dataList": dataList})
+    listRes= cur.fetchall()
 
 
-@csrf_exempt
-def query():
-    pass
+    for i in range(0,len(listRes)):
+        if(not listRes[i]['qtyzSum']):
+            listRes[i]['qtyzSum']=0
+        listRes[i]['qtyzSum'] = float(listRes[i]['qtyzSum'])
+        if(not listRes[i]['qtylSum']):
+            listRes[i]['qtylSum']=0
+        listRes[i]['qtylSum'] = float(listRes[i]['qtylSum'])
+        if(not listRes[i]['zhonbiSum']):
+            listRes[i]['zhonbiSum']=0
+        listRes[i]['zhonbiSum'] = float('%0.2f'%(listRes[i]['zhonbiSum']*100))
 
+        sql = "SELECT b.sdate,SUM(b.qtyz) qtyz , SUM(b.qtyl) qtyl, (SUM(b.qtyl)/SUM(b.qtyz)) zhonbi, (SELECT COUNT(DISTINCT zhonbi) FROM KNegativestock a WHERE a.zhonbi <= b.zhonbi) AS mingci " \
+              "FROM KNegativestock AS b " \
+              "WHERE ShopID ='" + listRes[i]['ShopID'] +"' AND sdate BETWEEN '"+monthFirst+"' AND '"+today+"' GROUP BY sdate"
 
-@csrf_exempt
-def download():
-    pass
+        cur.execute(sql)
+        listDetail = cur.fetchall()
+        for item in listDetail:
+            date = str(item['sdate'])[8:10]
+            if(not item['qtyz']):
+                listRes[i]['qtyz_'+date]=0
+            else:
+                listRes[i]['qtyz_'+date]=float(item['qtyz'])
+            if(not item['qtyl']):
+                listRes[i]['qtyl_'+date]=0
+            else:
+                listRes[i]['qtyl_'+date]=float(item['qtyl'])
+            if(not item['zhonbi']):
+                listRes[i]['zhonbi_'+date]=0
+            else:
+                listRes[i]['zhonbi_'+date]=float('%0.2f'%(item['zhonbi']*100))
+            listRes[i]['mingci_'+date]=item['mingci']
 
+    listRes = ranking(listRes,'zhonbiSum','mingciSum')
+    for date  in range(12,datetime.date.today().day+1):
+        if(date<10):
+            listRes = ranking(listRes,'zhonbi_0'+str(date),'mingci_0'+str(date))
+        else:
+            listRes = ranking(listRes,'zhonbi_'+str(date),'mingci_'+str(date))
+    return render(request,"report/daily/negative_stock_top.html",locals())
 
-def ranking(lis):
+def ranking(lis,key,name):
     """
     排名函数
     """
     # nums = [['a',11],['d',1],['g',34],['e',1],['h',35],['c',2],['i',37],['b',2],['f',1],['j',39]]
-    lis.sort(key=lambda x:x[4])
+
+    # for obj in lis:
+    #     for k in obj.keys():
+    #         item = obj[k]
+    #         if '%' in item :
+    #             item =float(item[0:len(item)-1])
+
+    lis.sort(key=lambda x:x[key])
     j = 1
     for i in range(0,len(lis)):
         if i > 0:
             a = lis[i-1]
             b = lis[i]
-            if float(a[4]) != float(b[4]):
+            if float(a[key]) != float(b[key]):
                 j += 1
-            b.append(j)
+            b[name]= j
+            a[key] = str(a[key])+'%'
         else:
             a = lis[i]
-            a.append(j)
+            a[name]= j
+            # a[key] = str(a[key])+'%'
     return lis
