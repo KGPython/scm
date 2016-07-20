2a4ccc0c14d862fb6d9b8e6b652e9d15b368a0e8
end-e <xiuliang555@foxmail.com>
2016-07-20 13:58:40 +0800

    xiul 修复两个project混乱问题

--- scm/wsgi.py
+++ scm/wsgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+
+from os.path import join, dirname, abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.insert(0, PROJECT_DIR)
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scm.settings")
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-sys.path.insert(0, BASE_DIR)
 os.environ["DJANGO_SETTINGS_MODULE"]="scm.settings"
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
