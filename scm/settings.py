d756a47c11f7854d928ab22cd5a2dd0073ad4ebd
chenxinguang1003 <583860027@qq.com>
2016-08-05 13:44:31 +0800

    数据配置

--- scm/settings.py
+++ scm/settings.py
@@ -93,6 +93,7 @@ SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_COOKIE_AGE=60*30  #30 minute
 
+
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 from base.utils import Constants
