cdb51199a9f40d69bc313305c8a0714da2aa0e66
ChenXinGuang1003 <583860027@qq.com>
2016-12-08 17:16:00 +0800

    负库存异常报表加缓存

--- base/report/abnormal/negprofit_lte200.py
+++ base/report/abnormal/negprofit_lte200.py
@@ -6,23 +6,30 @@ from django.views.decorators.csrf import csrf_exempt
 from base.utils import DateUtil,MethodUtil as mtu
 from base.models import Kgprofit,BasPurLog
 from django.http import HttpResponse
-import datetime,calendar,decimal
+import datetime,calendar,decimal,json
 import xlwt3 as xlwt
+from django.views.decorators.cache import cache_page
 
+
+def query(date):
+    karrs = {}
+    karrs.setdefault("bbdate", "{start}".format(start=date))
+    karrs.setdefault("profit__lte", "{profit}".format(profit=-200))
+    rlist = Kgprofit.objects.values("bbdate", "sdate", "shopid", "shopname", "goodsid", "goodsname", "deptid",
+                                    "deptname", "qty", "profit", "stockqty", "truevalue", "costvalue") \
+        .filter(**karrs).exclude(shopid='C009').order_by("bbdate", "shopid", "goodsid", "sdate")
+
+    formate_data(rlist)
+    data = {"rlist": list(rlist)}
+
+    return data
+
+@cache_page(60 * 2 ,key_prefix='abnormal_negprofit_lte_200')
 @csrf_exempt
 def index(request):
      yesterday = DateUtil.get_day_of_day(-1)
 
-     karrs = {}
-     karrs.setdefault("bbdate", "{start}".format(start=yesterday))
-     karrs.setdefault("profit__lte", "{profit}".format(profit=-200))
-     rlist = Kgprofit.objects.values("bbdate","sdate","shopid","shopname","goodsid","goodsname","deptid","deptname","qty","profit","stockqty","truevalue","costvalue")\
-                     .filter(**karrs).exclude(shopid='C009').order_by("bbdate","shopid","goodsid","sdate")
-
-     formate_data(rlist)
-
-     qtype = mtu.getReqVal(request,"qtype","1")
-
+     qtype = mtu.getReqVal(request, "qtype", "1")
      #操作日志
      if not qtype:
          qtype = "1"
@@ -33,24 +40,29 @@ def index(request):
      BasPurLog.objects.create(name="负毛利大于200",url=path,qtype=qtype,ucode=ucode,uname=uname,createtime=today)
 
      if qtype == "1":
-         return render(request, "report/abnormal/negprofit_lte200.html", {"rlist":list(rlist)})
+         data = query(yesterday)
+         return render(request, "report/abnormal/negprofit_lte200.html", data)
      else:
-         return export(rlist,yesterday)
+         fname = yesterday.strftime("%m.%d") + "_abnormal_negprofit_lte200.xls"
+         return export(fname,yesterday)
 
-def export(rlist,yesterday):
+import base.report.Excel as excel
 
+def export(fname,yesterday):
+    if not excel.isExist(fname):
+        data = query(yesterday)
+        createExcel(fname, data['rlist'])
+    res = {}
+    res['fname'] = fname
+    return HttpResponse(json.dumps(res))
+
+def createExcel(fname,data):
     wb = xlwt.Workbook(encoding='utf-8',style_compression=0)
-    #写入sheet1 门店
-    writeDataToSheet1(wb,rlist)
+    writeDataToSheet1(wb,data)
+    excel.saveToExcel(fname, wb)
 
-    outtype = 'application/vnd.ms-excel;'
-    fname = yesterday.strftime("%m.%d")+"negprofit_lte200"
 
-    response = mtu.getResponse(HttpResponse(),outtype,'%s.xls' % fname)
-    wb.save(response)
-    return response
-
-def writeDataToSheet1(wb,rlist):
+def writeDataToSheet1(wb,data):
     sheet = wb.add_sheet("负毛利大于200",cell_overwrite_ok=True)
 
     titles = [[("负毛利大于200" ,0,1,15)],
@@ -64,7 +76,7 @@ def writeDataToSheet1(wb,rlist):
     widthlist = [800,1000,800,800,2000,800,800,800,800,800,800,800,800,800,800]
 
     mtu.insertTitle2(sheet,titles,keylist,widthlist)
-    mtu.insertCell2(sheet,2,rlist,keylist,None)
+    mtu.insertCell2(sheet,2,data,keylist,None)
 
 def formate_data(rlist):
     for rows in rlist:
